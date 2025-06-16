86d566306932fa2857e2addda263aa3259806314
Demexis <devsor@inbox.lv>
2025-06-16 22:00:34 +0300

    feat: added new component - "BoolDelegate".

---
+++ Runtime/PrimitiveDelegates/BoolDelegate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    /// <summary>
+    /// Manipulates a boolean variable and its contextually associated events.<br/><br/>
+    /// Can be useful when building logic/behaviour via UnityEvent-s.
+    /// </summary>
+    public sealed class BoolDelegate : MonoBehaviour {
+        [field: SerializeField] public bool BoolValue { get; set; }
+
+        [field: SerializeField] public UnityEvent<bool> OnTrigger { get; set; } = new();
+        [field: SerializeField] public UnityEvent OnTriggerTrue { get; set; } = new();
+        [field: SerializeField] public UnityEvent OnTriggerFalse { get; set; } = new();
+        
+        public bool IsTrue => BoolValue;
+        public bool IsFalse => !BoolValue;
+
+        public void SetInvertValue(bool value) {
+            BoolValue = !value;
+        }
+
+        public void InvertValue() {
+            BoolValue = !BoolValue;
+        }
+
+        public void SetAndInvokeDelegate(bool value) {
+            BoolValue = value;
+            InvokeDelegate(value);
+        }
+        
+        public void InvokeDelegate() {
+            InvokeDelegate(BoolValue);
+        }
+        
+        public void InvokeDelegate(bool value) {
+            OnTrigger.Invoke(value);
+
+            if (value) {
+                OnTriggerTrue.Invoke();
+            } else {
+                OnTriggerFalse.Invoke();
+            }
+        }
+    }
+}
