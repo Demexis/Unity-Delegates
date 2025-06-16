b174e346991404607878d9d32ec702a46071c218
Demexis <devsor@inbox.lv>
2025-06-16 22:08:09 +0300

    feat: added new component - "OnDestroyDelegate". Renamed "EnableDisableDelegate" to "OnEnableDisableDelegate".

---
+++ Runtime/MonoBehaviourDelegates/OnDestroyDelegate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    /// <summary>
+    /// Invokes unity-event on <see cref="OnDestroy()"/>.
+    /// </summary>
+    public sealed class OnDestroyDelegate : MonoBehaviour {
+        [field: SerializeField] public UnityEvent OnDestroyed { get; set; } = new();
+
+        private void OnDestroy() {
+            OnDestroyed.Invoke();
+        }
+    }
+}
