7ce216af4ce701cabb30698e57a1cc89447ad820
Demexis <devsor@inbox.lv>
2025-06-18 15:51:24 +0300

    feat: added new component - "ActionDelegate".

---
+++ Runtime/LogicDelegates/ActionDelegate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    /// <summary>
+    /// An intermediate component to reduce duplicate subscriptions.
+    /// </summary>
+    public sealed class ActionDelegate : MonoBehaviour {
+        #if UNITY_EDITOR
+        [field: SerializeField, TextArea] public string EditorDescription { get; set; } = string.Empty;
+        #endif
+        [field: SerializeField] public UnityEvent OnTrigger { get; set; } = new();
+
+        public void Trigger() {
+            OnTrigger.Invoke();
+        }
+    }
+}
