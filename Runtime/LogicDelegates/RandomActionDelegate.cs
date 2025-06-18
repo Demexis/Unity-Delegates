77450f2f16cf0a5f246a9abaa4d0ecf711b6db1b
Demexis <devsor@inbox.lv>
2025-06-18 15:52:15 +0300

    feat: added new component - "RandomActionDelegate".

---
+++ Runtime/LogicDelegates/RandomActionDelegate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    /// <summary>
+    /// Performs action with random chance.
+    /// </summary>
+    public sealed class RandomActionDelegate : MonoBehaviour {
+        private const float MIN_CHANCE = 0f;
+        private const float MAX_CHANCE = 1f;
+        
+        [field: SerializeField, Range(MIN_CHANCE, MAX_CHANCE)] public float Chance { get; set; } = 0.5f;
+        [field: SerializeField] public UnityEvent OnSuccess { get; set; } = new();
+        [field: SerializeField] public UnityEvent OnFailure { get; set; } = new();
+        
+        public void Trigger() {
+            Trigger(Chance);
+        }
+        
+        public void Trigger(float chance) {
+            var rand = Random.Range(MIN_CHANCE, MAX_CHANCE);
+
+            if (rand > chance) {
+                OnFailure.Invoke();
+                return;
+            }
+            
+            OnSuccess.Invoke();
+        }
+    }
+}
