7c0f2930ae72a8478d099c0e0c1d6c2bbb4fb98b
Demexis <devsor@inbox.lv>
2025-06-18 21:16:20 +0300

    feat: added new component - "AnimationCurvePlayerDelegate".

---
+++ Runtime/LogicDelegates/AnimationCurvePlayerDelegate.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    public sealed class AnimationCurvePlayerDelegate : MonoBehaviour {
+        [field: SerializeField] public AnimationCurve AnimationCurve { get; set; } = new();
+        [field: SerializeField] public bool IsPlaying { get; set; }
+        [field: SerializeField] public bool IsLooping { get; set; }
+
+        [field: SerializeField] public float CurveTime { get; set; } = 1f;
+        [field: SerializeField] public float EvaluatedValueScale { get; set; } = 1f;
+
+        [field: SerializeField] public UnityEvent<float> OnCurveEvaluated { get; set; } = new();
+        
+        public float CurveTimer { get; set; } 
+
+        public void Play() {
+            IsPlaying = true;
+        }
+
+        public void Stop() {
+            IsPlaying = false;
+            CurveTimer = 0f;
+        }
+        
+        private void Update() {
+            if (!IsPlaying) {
+                return;
+            }
+
+            if (CurveTimer >= CurveTime) {
+                if (!IsLooping) {
+                    IsPlaying = false;
+                    return;
+                }
+                
+                CurveTimer -= CurveTime;
+            }
+            
+            CurveTimer += Time.deltaTime;
+
+            var time = Mathf.Clamp(CurveTimer / CurveTime, 0f, AnimationCurve[AnimationCurve.length - 1].time);
+
+            var evaluated = AnimationCurve.Evaluate(time) * EvaluatedValueScale;
+            
+            OnCurveEvaluated.Invoke(evaluated);
+        }
+    }
+}
