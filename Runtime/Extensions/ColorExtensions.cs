82fd0553a08cd61f2784b1fac74a1691789ee95a
Demexis <devsor@inbox.lv>
2025-06-18 15:50:53 +0300

    feat: added new component - "ColorDelegate".

---
+++ Runtime/Extensions/ColorExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.Contracts;
+using UnityEngine;
+
+namespace Demegraunt.Framework {
+    public static class ColorExtensions {
+        [Pure]
+        public static Color SetR(this Color color, float r) {
+            return new Color(r, color.g, color.b, color.a);
+        }
+
+        [Pure]
+        public static Color SetG(this Color color, float g) {
+            return new Color(color.r, g, color.b, color.a);
+        }
+
+        [Pure]
+        public static Color SetB(this Color color, float b) {
+            return new Color(color.r, color.g, b, color.a);
+        }
+
+        [Pure]
+        public static Color SetA(this Color color, float a) {
+            return new Color(color.r, color.g, color.b, a);
+        }
+        
+        [Pure]
+        public static Color MultiplyR(this Color color, float r) {
+            return new Color(color.r * r, color.g, color.b, color.a);
+        }
+        
+        [Pure]
+        public static Color MultiplyG(this Color color, float g) {
+            return new Color(color.r, color.g * g, color.b, color.a);
+        }
+
+        [Pure]
+        public static Color MultiplyB(this Color color, float b) {
+            return new Color(color.r, color.g, color.b * b, color.a);
+        }
+
+        [Pure]
+        public static Color MultiplyA(this Color color, float a) {
+            return new Color(color.r, color.g, color.b, color.a * a);
+        }
+        
+        [Pure]
+        public static Color SetHue(this Color color, float hue) {
+            Color.RGBToHSV(color, out _, out var s, out var v);
+            return Color.HSVToRGB(hue, s, v);
+        }
+        
+        [Pure]
+        public static Color SetSaturation(this Color color, float saturation) {
+            Color.RGBToHSV(color, out var h, out _, out var v);
+            return Color.HSVToRGB(h, saturation, v);
+        }
+        
+        [Pure]
+        public static Color SetValue(this Color color, float value) {
+            Color.RGBToHSV(color, out var h, out var s, out _);
+            return Color.HSVToRGB(h, s, value);
+        }
+    }
+}
---
+++ Runtime/ValueTypeDelegates/ColorDelegate.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    public sealed class ColorDelegate : MonoBehaviour {
+        [field: SerializeField] public Color Color { get; set; } = Color.white;
+        [field: SerializeField] public UnityEvent<Color> OnTrigger { get; set; } = new();
+
+        public void SetR(float r) {
+            Color = Color.SetR(r);
+        }
+        
+        public void SetG(float g) {
+            Color = Color.SetG(g);
+        }
+        
+        public void SetB(float b) {
+            Color = Color.SetB(b);
+        }
+        
+        public void SetA(float a) {
+            Color = Color.SetA(a);
+        }
+        
+        public void SetHue(float hue) {
+            Color = Color.SetHue(hue);
+        }
+        
+        public void SetSaturation(float saturation) {
+            Color = Color.SetSaturation(saturation);
+        }
+        
+        public void SetValue(float value) {
+            Color = Color.SetValue(value);
+        }
+        
+        public void MultiplyR(float r) {
+            Color = Color.MultiplyR(r);
+        }
+        
+        public void MultiplyG(float g) {
+            Color = Color.MultiplyG(g);
+        }
+        
+        public void MultiplyB(float b) {
+            Color = Color.MultiplyB(b);
+        }
+        
+        public void MultiplyA(float a) {
+            Color = Color.MultiplyA(a);
+        }
+
+        public void Trigger() {
+            Trigger(Color);
+        }
+
+        public void Trigger(Color color) {
+            OnTrigger.Invoke(color);
+        }
+    }
+}
