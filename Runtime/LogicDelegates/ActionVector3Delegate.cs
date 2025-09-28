ca01d74b1a47118111348346741b788c3e009afa
Demexis <devsor@inbox.lv>
2025-09-28 17:53:36 +0300

    feat: added ActionVector3Delegate.

---
+++ Runtime/LogicDelegates/ActionVector3Delegate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Demegraunt.Framework {
+    public sealed class ActionVector3Delegate : MonoBehaviour {
+        [field: SerializeField] public Vector3 Vec3 { get; set; } = Vector3.zero;
+        [field: SerializeField] public UnityEvent<Vector3> OnTrigger { get; set; } = new();
+
+        public void SetAndTrigger(Vector3 vec3) {
+            Vec3 = vec3;
+            Trigger();
+        }
+        
+        public void Trigger() {
+            OnTrigger.Invoke(Vec3);
+        }
+    }
+}
